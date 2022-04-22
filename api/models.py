74c896953bfbf6cb17ff5245896a275fb717a55d
Tatsuyyy <aderybase910@gmail.com>
2022-04-22 23:37:27 +0900

    本番用アプリケーション作成

---
+++ api/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Arrangements(models.Model):
+    task = models.OneToOneField('Tasks', models.DO_NOTHING, primary_key=True)
+    weekday = models.ForeignKey('Weekdays', models.DO_NOTHING)
+    required_per_day = models.SmallIntegerField(blank=True, null=True)
+    done_per_day = models.SmallIntegerField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'arrangements'
+        unique_together = (('task', 'weekday'),)
+
+
+class Students(models.Model):
+    name = models.CharField(max_length=50)
+    teacher = models.ForeignKey('Teachers', models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'students'
+
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    required_time = models.SmallIntegerField()
+    is_done = models.BooleanField()
+    student = models.ForeignKey(Students, models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'tasks'
+
+
+class Teachers(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'teachers'
+
+
+class Weekdays(models.Model):
+    name = models.CharField(max_length=1)
+
+    class Meta:
+        db_table = 'weekdays'
--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'sample_api.apps.SampleApiConfig',
+    'api.apps.ApiConfig',
     'drf_spectacular'
 ]
 
